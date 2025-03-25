33533b5ee33453d0f066d6df5c97f0b5f0245ca6
AbdullahZuhair0 <abdullahzuhairmc@gmail.com>
2025-03-25 21:20:27 +0400

    updated mm-matches

--- mm-matches.s
+++ mm-matches.s
@@ -1,6 +1,6 @@
 @ This ARM Assembler code should implement a matching function, for use in the MasterMind program, as
 @ described in the CW2 specification. It should produce as output 2 numbers, the first for the
-@ exact matches (peg of right colour and in right position) and approximate matches (peg of right
+@ exact compare (peg of right colour and in right position) and approximate compare (peg of right
 @ color but not in right position). Make sure to count each peg just once!
 	
 @ Example (first sequence is secret, second sequence is guess):
@@ -17,16 +17,13 @@
 @ use the name `main` here, for standalone testing of the assembler code
 @ when integrating this code into `master-mind.c`, choose a different name
 @ otw there will be a clash with the main function in the C code
-.global         loader
-loader: 
-	LDR  r2, =secret	@ pointer to secret sequence
-	LDR  r3, =guess		@ pointer to guess sequence
-	mov r4, #0 @ approx. matches
-	mov r5, #0 @ exact matches
+matches: 
+	mov r4, #0 @ approx. compare
+	mov r5, #0 @ exact compare
 	mov r6, #0 @ i
 	mov r8, #0 @ j 
 	mov r9, #4 @ new number
-	b matches
+	b compare
 	
 
 
@@ -39,7 +36,7 @@ exit:
 @ sub-routines
 
 @ this is the matching fct that should be callable from C	
-matches:			@ Input: R0, R1 ... ptr to int arrays to match ; Output: R0 ... exact matches (10s) and approx matches (1s) of base COLORS
+compare:			@ Input: R0, R1 ... ptr to int arrays to match ; Output: R0 ... exact compare (10s) and approx compare (1s) of base COLORS
 		cmp r6, #3
 		beq addmatches
 		bl access
@@ -50,52 +47,52 @@ exact:
 	 	add r5, r5, #10 @exact + 10
 		add r6, r6, #1 @ i++
 		mov r9, #1 
-		add r2, r9, lsl #2 @ inc r2
+		add r0, r9, lsl #2 @ inc r0
 		mov r9, #4
-		str r9, [r3] @ r3[current] = 4
-		add r3, r9, lsl #2 @ inc r2
-		b matches
+		str r9, [r1] @ r1[current] = 4
+		add r1, r9, lsl #2 @ inc r0
+		b compare
 approx:
-		sub r3, r6, lsl #2 @ r3[i] -> r3[0]
+		sub r1, r6, lsl #2 @ r1[i] -> r1[0]
 		bl access
 		cmp r11, r12
 		beq incApprox
 		add r10, r10, #1 @ j++
 		mov r9, #1
-		add r3, r9, lsl #2 @ inc r3
+		add r1, r9, lsl #2 @ inc r1
 		bl access
 		cmp r11, r12
 		beq incApprox
 		add r10, r10, #1 @ j++
-		add r3, r9, lsl #2 @ inc r3
+		add r1, r9, lsl #2 @ inc r1
 		bl access
 		cmp r11, r12
 		beq incApprox
-		@ no matches at all go back
+		@ no compare at all go back
 		mov r10, #0 @ j = 0
 		mov r9, #2
-		sub r3, r9, lsl #2 @ set r3[2] back to r3[0]
-		add r3, r6, lsl #2 @ set r3 back to r3[i]
+		sub r1, r9, lsl #2 @ set r1[2] back to r1[0]
+		add r1, r6, lsl #2 @ set r1 back to r1[i]
 		add r6, r6, #1 @ i++
-		b matches
+		b compare
 incApprox: 
 		mov r9, #4
-		str r9, [r3] @ r3[j] = 4
-		sub r3, r10, lsl #2 @ set r3[j] back to r3[0]
+		str r9, [r1] @ r1[j] = 4
+		sub r1, r10, lsl #2 @ set r1[j] back to r1[0]
 		mov r10, #0 @ j = 0
 		add r4, r4, #1 @ approx++
 		add r6, r6, #1 @ i++
 		mov r9, #1
-		add r3, r6, lsl #2 @ r3[0] -> r3[i]
-		add r2, r9, lsl #2 @ r2[i-1] -> r2[i]
-		b matches
+		add r1, r6, lsl #2 @ r1[0] -> r1[i]
+		add r0, r9, lsl #2 @ r0[i-1] -> r0[i]
+		b compare
 addmatches:
 		add r5, r5, r4 @ r5 = exact + approx
 		mov r0, r5 @ r0 = r5
 		b exit
 access:
-		ldr r11, [r2]
-		ldr r12, [r3]
+		ldr r11, [r0]
+		ldr r12, [r1]
 		bx lr
 .data
 
